b49692ecfe47e32933546f0a31eb223297524254
BC215 <dlqudckd3@gmail.com>
2026-06-09 17:42:34 +0900

    제거

--- backend/src/main/java/com/moodcast/service/FileUploadService.java
+++ backend/src/main/java/com/moodcast/service/FileUploadService.java
@@ -1,6 +1,5 @@
 package com.moodcast.service;
 
-import com.moodcast.service.dto.FileUploadResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
